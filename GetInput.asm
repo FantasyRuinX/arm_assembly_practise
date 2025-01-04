ea49608a62728414851d093d2a3eea5094dd1370
sbayat023 <sbayat023@student.wethinkcode.co.za>
2025-01-04 20:15:24 +0200

    Updated code

--- GetInput.asm
+++ GetInput.asm
@@ -1,18 +1,33 @@
 
 .section .data
 buffer : .space 100
-msg : .asciz "Inputed : \n"
+userPrompt : .asciz "Please enter a message to echo : "
+msg : .asciz "Inputed : "
 
 .global _start
 .section .text
 
 _start:
 
+    @Prompt user for input
+    mov r7,#4
+    mov r0,#1
+    ldr r1, =userPrompt
+    mov r2,#33
+    svc #0
+
     @System call for user input
     mov r7,#3
     mov r0,#0
     ldr r1,=buffer
     mov r2,#100
+    svc #0
+
+    @System call to print user input 
+    mov r7,#4
+    mov r0,#1
+    ldr r1, =msg
+    mov r2,#10
     svc #0
 
     @System call to print user input 
