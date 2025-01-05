900fa5e019d711d49711266b6a327e9f10e9599d
sbayat023 <samuelbayat707@gmail.com>
2025-01-05 19:01:50 +0200

    Updated RunFile.bash to check if packages are installed

--- RunFile.bash
+++ RunFile.bash
@@ -3,6 +3,15 @@
 echo "Enter file name (eg HelloWorld): "
 read tempName
 
-arm-none-eabi-as -o "$tempName.o" "$tempName.asm"
-arm-none-eabi-ld -o "$tempName" "$tempName.o"
-qemu-arm "./$tempName"
+if arm-none-eabi-gcc --version | grep "arm-none-eabi-gcc"; then
+    arm-none-eabi-as -o "$tempName.o" "$tempName.asm"
+    arm-none-eabi-ld -o "$tempName" "$tempName.o"
+else
+    echo "Please install arm-none-eabi-gcc to use this script"
+fi
+
+if qemu-arm --version | grep "qemu-arm"; then
+    qemu-arm "./$tempName"
+else
+    echo "Please install qemu-arm to use this script"
+fi
